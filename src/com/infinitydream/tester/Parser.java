c82c9d91d0a55b60aff67ef9e974e140d25a2482
Mohamed Mayla <mohamedmayla@gmail.com>
2014-11-30 16:39:53 +0200

    formating and fixing indentation

--- src/com/infinitydream/tester/Parser.java
+++ src/com/infinitydream/tester/Parser.java
@@ -1,4 +1,5 @@
 package com.infinitydream.tester;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -6,47 +7,39 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
-	
-	
-    private File text ;
-    private Scanner sc ;
+
+    private File text;
+    private Scanner sc;
     private List<TestSet> ListTestSet = new ArrayList<TestSet>();
-    TestSet T ;
-    
-    
-	public Parser ()
-	{
-		
+    TestSet T;
+
+    public Parser() {
+
+    }
+
+    public void Parse(String path) {
+	text = new File(path);
+	try {
+	    sc = new Scanner(text);
+	    while (sc.hasNextLine()) {
+		T = new TestSet();
+		T.setHandle(sc.nextLine());
+		T.setEqnSequence(sc.nextLine().replaceAll("\\s", "")
+			.toCharArray());
+		T.setExpectedResults(sc.nextLine());
+
+		ListTestSet.add(T);
+	    }
 	}
-	
-	public void Parse(String path)
-	{
-		text = new File (path);
-		try 
-		{
-			sc = new Scanner(text);
-			while(sc.hasNextLine())
-			{
-				T = new TestSet();
-				T.setHandle(sc.nextLine());
-				T.setEqnSequence(sc.nextLine().replaceAll("\\s", "").toCharArray());
-				T.setExpectedResults(sc.nextLine());
-				
-				ListTestSet.add(T);
-			}
-		} 
-		
-		catch (FileNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
+	catch (FileNotFoundException e) {
+	    e.printStackTrace();
 	}
-	public List<TestSet> getListTestSet()
-	{
-		return this.ListTestSet;
-	}
-	
+
+    }
+
+    public List<TestSet> getListTestSet() {
+	return this.ListTestSet;
+    }
 
 }
