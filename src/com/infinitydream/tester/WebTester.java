51478180d5d9d8aa765acda6f392302e2d8a3d66
Raneem_medhat <raneem.medhat@gmail.com>
2014-12-01 11:16:50 +0200

    WebTester fully implemented. Don't forget to change tha path of the website and the test sets.

--- src/com/infinitydream/tester/WebTester.java
+++ src/com/infinitydream/tester/WebTester.java
@@ -1,6 +1,15 @@
 package com.infinitydream.tester;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
+
+
+
 
 import org.junit.*;
 
@@ -12,27 +21,73 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 @SuppressWarnings("unused")
 public class WebTester {
+	  
+	  private Parser P = new Parser();
+	  private List<TestSet> currList = new ArrayList<TestSet>();
+		
+	//Path path = Paths.get("");
 	  private WebDriver driver;
 	  @Before
 	  public void setUp() throws Exception {
 	    driver = new FirefoxDriver();
 	  }
-	// Search using keyword through Google search
+	 
+	  // get list of Test Sets from Parser 
+	  @Before
+	  public void setUpList() throws Exception {
+		  //Add the testcase file path here
+		  P.Parse("C:\\Users\\Raneem\\git\\calculator-tester\\testcase.txt");
+		  currList = P.getListTestSet();
+		  }
 
 	  @Test
-	  public void testtestclass() throws Exception {
-	        //Open Home Page
-	        driver.get("http://www.google.com.eg");
-	        //Enter text in search box
-	        driver.findElement(By.name("q")).sendKeys("selenium");
-	        Thread.sleep(10000);
-	        //Click Enter to Search
-	        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-	        Thread.sleep(10000);
+	  public void testList() throws Exception {
+		  // change it to your local host till we host it on a free host online
+	
+		  //Open page
+		  driver.get("file:///C:/Users/Raneem/git/calculator-tester/WebCalculator/index.html");
+		  
+		  TestSet T= new TestSet();
+		  char[] eqn;
+		  WebElement W;
+		  for (int i=0; i<currList.size(); i++)
+		  {
+			  T=currList.get(i);
+			  eqn=T.getEqnSequence();
+			  String chid;
+			  for (int j=0;j<eqn.length; j++)
+			  {
+				  chid =Character.toString(eqn[j]);
+				  try
+				  {
+				  driver.findElement(By.id(chid)).click();
+				  }
+				  catch (NoSuchElementException e)
+				  {
+					  
+				  }
+			  }
+			
+			  driver.findElement(By.id("=")).click();
+		     
+			  //Save Results
+		      W= driver.findElement(By.className("results"));
+			  T.setActualResults(W.getText());
+			  if(T.getExpectedResults()==T.getActualResults())
+			  {
+				  T.setSuccess(true);  	  
+			  }
+			  currList.set(i, T);
+			  driver.findElement(By.id("AC")).click();
+		  }
 	  }
 
 	  @After
 	  public void tearDown() throws Exception {
 	    driver.quit();
 	    }
+	  
+	    public List<TestSet> getcurrList() {
+			return this.currList;
+		    }
 	  }
